3d462ff332d89668345e80c6552b67833adebce3
RomarioNik <115925888+RomarioNik@users.noreply.github.com>
2023-07-15 09:31:47 +0100

    add try-catch to contacts

--- contacts.js
+++ contacts.js
@@ -4,45 +4,64 @@ import { nanoid } from "nanoid";
 
 const contactsPath = path.resolve("db", "contacts.json");
 
-const updateContacts = contacts => fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+const updateContacts = (contacts) =>
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
 const listContacts = async () => {
+  try {
     const contacts = await fs.readFile(contactsPath);
     return JSON.parse(contacts);
-}
+  } catch (e) {
+    console.log(e.message);
+  }
+};
 
 const getContactById = async (contactId) => {
+  try {
     const contacts = await listContacts();
-    const contactItem = contacts.find(item => item.id === contactId);
+    const contactItem = contacts.find((item) => item.id === contactId);
 
     if (!contactItem) return null;
 
     return contactItem;
-}
+  } catch (e) {
+    console.log(e.message);
+  }
+};
 
 const removeContact = async (contactId) => {
+  try {
     const contacts = await listContacts();
-    const contactIndex = await contacts.findIndex(item => item.id === contactId);
+    const contactIndex = await contacts.findIndex(
+      (item) => item.id === contactId
+    );
 
     if (contactIndex === -1) return null;
 
     const [deletedContact] = contacts.splice(contactIndex, 1);
     await updateContacts(contacts);
     return deletedContact;
-}
+  } catch (e) {
+    console.log(e.message);
+  }
+};
 
-const addContact = async ({name, email, phone}) => {
+const addContact = async ({ name, email, phone }) => {
+  try {
     const contacts = await listContacts();
     const newContact = {
-        "id": nanoid(),
-        name,
-        email,
-        phone,
-    }
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
 
     contacts.push(newContact);
     await updateContacts(contacts);
     return newContact;
-}
+  } catch (e) {
+    console.log(e.message);
+  }
+};
 
-export default {listContacts, getContactById, removeContact, addContact};
+export default { listContacts, getContactById, removeContact, addContact };
